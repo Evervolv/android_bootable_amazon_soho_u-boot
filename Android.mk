12ae10ff7ce8c6cd7bd42e9b966d3d8faf33a247
Saleem Rashid <dev@saleemrashid.com>
2015-12-27 16:00:39 +0000

    exploit: Initial in-tree build

--- Android.mk
+++ Android.mk
@@ -2,22 +2,22 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(strip $(LOCAL_PATH)),$(TARGET_UBOOT_DIR))
 
-TARGET_UBOOT := $(PRODUCT_OUT)/u-boot.bin
+INSTALLED_UBOOT_TARGET := $(PRODUCT_OUT)/u-boot.bin
 UBOOT_OUT := $(abspath $(TARGET_OUT_INTERMEDIATES)/u-boot)
 UBOOT_MAKE := $(MAKE) -C $(UBOOT_OUT) CROSS_COMPILE=arm-eabi-
 
 .PHONY: u-boot
-u-boot: $(TARGET_UBOOT)
+u-boot: $(INSTALLED_UBOOT_TARGET)
 all_modules: u-boot
 
-$(TARGET_UBOOT): $(UBOOT_OUT)/u-boot.bin
+$(INSTALLED_UBOOT_TARGET): $(UBOOT_OUT)/u-boot.bin
 	$(hide) cp $< $@
 
 $(UBOOT_OUT)/u-boot.bin: $(UBOOT_OUT) $(UBOOT_OUT)/include/config.h
 	$(hide) $(UBOOT_MAKE)
 
 $(UBOOT_OUT)/include/config.h:
-	$(hide) $(UBOOT_MAKE) $(TARGET_UBOOT_CONFIG)
+	$(UBOOT_MAKE) $(TARGET_UBOOT_CONFIG)
 
 $(UBOOT_OUT): $(LOCAL_PATH)
 	$(hide) cd $(TARGET_UBOOT_DIR); find . -type f -exec sh -c 'mkdir -p "$(UBOOT_OUT)/$$(dirname {})"; ln -sf "$(abspath $(TARGET_UBOOT_DIR))/{}" "$(UBOOT_OUT)/{}"' \;
