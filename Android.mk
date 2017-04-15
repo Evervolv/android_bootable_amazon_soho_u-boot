16d26e0e0286693f4a753ba6f00fbd74ef3af0d2
William Bellavance <flintman@flintmancomputers.com>
2017-04-14 22:40:12 -0400

    fix up building

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@ ifeq ($(strip $(LOCAL_PATH)),$(TARGET_UBOOT_DIR))
 
 INSTALLED_UBOOT_TARGET := $(PRODUCT_OUT)/u-boot.bin
 UBOOT_OUT := $(abspath $(TARGET_OUT_INTERMEDIATES)/u-boot)
-UBOOT_MAKE := $(MAKE) -C $(UBOOT_OUT) CROSS_COMPILE=arm-eabi-
+UBOOT_MAKE := $(MAKE) -C $(UBOOT_OUT) ARCH=arm CROSS_COMPILE=arm-eabi-
 
 .PHONY: u-boot
 u-boot: $(INSTALLED_UBOOT_TARGET)
@@ -20,7 +20,7 @@ $(UBOOT_OUT)/include/config.h: $(UBOOT_OUT)
 	$(hide) $(UBOOT_MAKE) $(TARGET_UBOOT_CONFIG)
 
 $(UBOOT_OUT): $(LOCAL_PATH)
-	$(hide) cd $(TARGET_UBOOT_DIR); find . -type f -exec sh -c 'mkdir -p "$(UBOOT_OUT)/$$(dirname {})"; ln -sf "$(abspath $(TARGET_UBOOT_DIR))/{}" "$(UBOOT_OUT)/{}"' \;
+	$(hide) cd $(TARGET_UBOOT_DIR); find . -type f \( -exec sh -c 'mkdir -p "$(UBOOT_OUT)/$$(dirname {})"; ln -sf "$(abspath $(TARGET_UBOOT_DIR))/{}" "$(UBOOT_OUT)/{}"' \; \)
 	$(hide) rm $(UBOOT_OUT)/Android.mk
 
 endif
